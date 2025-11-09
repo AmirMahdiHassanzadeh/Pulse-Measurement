f144862f2761a3bfbaa239c9bd8e7dc0ebb89f2e
AmirMahdi <81297833+AmirMahdiHassanzadeh@users.noreply.github.com>
2025-11-09 23:40:41 +0330

    change Done T

--- PulseMeasurement.vhd
+++ PulseMeasurement.vhd
@@ -1,158 +1,114 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
-entity PulseMeasurement is
-  Port (
-    iClk          : in STD_LOGIC;
-    iNReset       : in STD_LOGIC;
-    iPulse        : in STD_LOGIC;
-    iEnable       : in STD_LOGIC;
-    oHalfDone     : out STD_LOGIC;
-    oDone         : out STD_LOGIC;
-    oFirstEdge    : out STD_LOGIC;
-    oHighDuration : out STD_LOGIC_VECTOR(31 downto 0);
-    oLowDuration  : out STD_LOGIC_VECTOR(31 downto 0);
-    oPeriod       : out STD_LOGIC_VECTOR(31 downto 0)
-  );
-end PulseMeasurement;
-
 architecture Behavioral of PulseMeasurement is
- 
-  signal rHighDuration    : INTEGER := 0;
-  signal rLowDuration     : INTEGER := 0;
-  signal rPeriod          : INTEGER := 0;
-  signal rCounter         : INTEGER := 0;
-  signal rEdgeCounter     : INTEGER Range 0 to 2:= 0;
- 
-  signal rEnable          : STD_LOGIC := '0';
-  signal pulse_stable     : STD_LOGIC := '0';
-  signal rFirstEdge       : STD_LOGIC := '0';
-  signal IsFirstEdge      : STD_LOGIC := '0';
-  signal pulse_stableLast : STD_LOGIC := '0';
-  signal rMeasureDone     : STD_LOGIC := '0';
-  signal rMeasureHalfDone : STD_LOGIC := '0';
-  signal rMeasureDone_H   : STD_LOGIC := '0';
-  signal rMeasureDone_L   : STD_LOGIC := '0';
---------------------------------------------------
-signal pulse_sync1, pulse_sync2 : std_logic := '0';
-signal pulse_stable : std_logic := '0';
+
+  constant PULSE_WIDTH : integer := 4; -- چند سیکل پالس دیده بشه
+
+  -- شمارنده‌ها و متغیرها
+  signal rHighDuration, rLowDuration, rPeriod, rCounter : integer := 0;
+  signal rEdgeCounter : integer range 0 to 2 := 0;
+
+  signal pulse_sync1, pulse_sync2, pulse_stable, pulse_last : std_logic := '0';
+  signal rFirstEdge, IsFirstEdge : std_logic := '0';
+
+  -- خروجی‌ها
+  signal rHalfDone, rDone : std_logic := '0';
+
+  -- شمارنده‌ی کشش پالس‌ها
+  signal half_cnt, done_cnt : integer range 0 to PULSE_WIDTH := 0;
 
 begin
---------------------------------------------------
-  rEnable <= iEnable;
+  oHalfDone <= rHalfDone;
+  oDone     <= rDone;
   oFirstEdge <= rFirstEdge;
-  oDone <= rMeasureDone;
-  oHalfDone <= rMeasureHalfDone;
-  oHighDuration <= std_logic_vector(to_unsigned(rHighDuration,32));
-  oLowDuration <= std_logic_vector(to_unsigned(rLowDuration,32));
-  oPeriod <= std_logic_vector(to_unsigned(rPeriod,32));
-  pulse_stable <= pulse_sync2;
----------------------------------------------------------------------
-  process(iClk)
+
+  oHighDuration <= std_logic_vector(to_unsigned(rHighDuration, 32));
+  oLowDuration  <= std_logic_vector(to_unsigned(rLowDuration, 32));
+  oPeriod       <= std_logic_vector(to_unsigned(rPeriod, 32));
+
+  -------------------------------------------------------------------
+  process(iClk, iNReset)
   begin
-    if rising_edge(iClk) then
+    if iNReset = '0' then
+      pulse_sync1 <= '0';
+      pulse_sync2 <= '0';
+      pulse_stable <= '0';
+      pulse_last <= '0';
+      rHighDuration <= 0;
+      rLowDuration <= 0;
+      rCounter <= 0;
+      rPeriod <= 0;
+      rEdgeCounter <= 0;
+      IsFirstEdge <= '0';
+      rFirstEdge <= '0';
+      rHalfDone <= '0';
+      rDone <= '0';
+      half_cnt <= 0;
+      done_cnt <= 0;
+
+    elsif rising_edge(iClk) then
+
+      -- سینکرون‌سازی
       pulse_sync1 <= iPulse;
       pulse_sync2 <= pulse_sync1;
-    end if;
-  end process;
+      pulse_stable <= pulse_sync2;
 
----------------------------------------------------------------------
-  WhatIsEdge:process(iClk, iNReset)
-    begin
-    if iNReset = '0' then
-    
-      rFirstEdge <='0';
-      IsFirstEdge <='0';
-      pulse_stableLast <= pulse_stableLast;
-      
-    elsif rising_edge(iClk) then
-    
-      pulse_stableLast <= pulse_stable;
-      
-      if rEnable = '1' then
-        if (pulse_stable = '1') and (pulse_stableLast = '0') then --Rising Edge Pulse
-        
+      -- پیش‌فرض صفر
+      if half_cnt = 0 then
+        rHalfDone <= '0';
+      else
+        half_cnt <= half_cnt - 1;
+      end if;
+
+      if done_cnt = 0 then
+        rDone <= '0';
+      else
+        done_cnt <= done_cnt - 1;
+      end if;
+
+      if iEnable = '1' then
+        rCounter <= rCounter + 1;
+
+        -- لبه بالا
+        if (pulse_stable = '1') and (pulse_last = '0') then
           if IsFirstEdge = '0' then
             IsFirstEdge <= '1';
             rFirstEdge <= '1';
-           
           end if;
-         
-        elsif (pulse_stable = '0') and (pulse_stableLast = '1') then --Falling Edge Pulse
-         
+
+          rLowDuration <= rCounter;
+          rCounter <= 0;
+          rEdgeCounter <= rEdgeCounter + 1;
+          rHalfDone <= '1';
+          half_cnt <= PULSE_WIDTH;
+
+        -- لبه پایین
+        elsif (pulse_stable = '0') and (pulse_last = '1') then
           if IsFirstEdge = '0' then
             IsFirstEdge <= '1';
             rFirstEdge <= '0';
           end if;
-        end if;
-        
-      else --Enable = '0'
-        IsFirstEdge <= '0';
-      end if;--Enable = '1'
-    end if; --rising_edge(iClk)
-  end process;
 
----------------------------------------------------------------------
-  WhatIsDuration:process(iClk, iNReset)
-    begin
-    if iNReset = '0' then
-
-      rLowDuration <= 0;
-      rHighDuration <= 0;
-      rCounter <= 1;
-      rPeriod <= 0;
-
-      rMeasureDone <='0';
-      rMeasureHalfDone <='0';
-      rMeasureDone_H <='0';
-      rMeasureDone_L <='0';
-      rEdgeCounter <= 0;
-
-    elsif rising_edge(iClk) then
-
-      rMeasureHalfDone <= '0';
-      rMeasureDone <= '0';
-
-      if rEnable = '1' then
-        if (pulse_stable = '1') and (pulse_stableLast = '0') and (IsFirstEdge = '1') then --Rising Edge Pulse
-          rCounter <= 1;
-          rMeasureDone_H <= '1';
-          rLowDuration <= rCounter;
-          rEdgeCounter <= rEdgeCounter + 1;
-
-        elsif (pulse_stable = '0') and (pulse_stableLast = '1')and (IsFirstEdge = '1') then --Falling Edge Pulse
-          rCounter <= 1;
-          rMeasureDone_L <= '1';
           rHighDuration <= rCounter;
+          rCounter <= 0;
           rEdgeCounter <= rEdgeCounter + 1;
-
-        elsif pulse_stable = pulse_stableLast then --Pulse level Duration
-          rCounter <= rCounter + 1;
-          rMeasureDone_H <= '0';
-          rMeasureDone_L <= '0';
-        end if;
-
-        if rMeasureDone_L = '1' or rMeasureDone_H = '1' then
-            rMeasureHalfDone <= '1';
+          rHalfDone <= '1';
+          half_cnt <= PULSE_WIDTH;
         end if;
 
         if rEdgeCounter = 2 then
-          rMeasureDone <= '1';
           rPeriod <= rHighDuration + rLowDuration;
           rEdgeCounter <= 0;
+          rDone <= '1';
+          done_cnt <= PULSE_WIDTH;
         end if;
+
       else
+        rCounter <= 0;
         rEdgeCounter <= 0;
-      end if;--Enable = '1'
-    end if; --rising_edge(iClk)
+        IsFirstEdge <= '0';
+        rFirstEdge <= '0';
+      end if;
+
+      pulse_last <= pulse_stable;
+    end if;
   end process;
----------------------------------------------------------------------
- 
 end Behavioral;
